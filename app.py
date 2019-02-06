d798a0dad23d0148855ede5093edf49cbd3a7fb5
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-02-06 16:13:34 -0500

    added followers

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-"""use this for main page info // response = requests.get("https://api.github.com/users/ChristinaDRoberts")
-use this for repository page //response = requests.get("https://api.github.com/users/ChristinaDRoberts/repos")"""
-
-
 import csv
 
 from flask import Flask
@@ -10,12 +6,12 @@ from flask import render_template
 
 import requests
 
+# load all the packages
+app = Flask(__name__)   # load all the packages ??? where does it look for packages
+
+# this says if the search bar shows /(just base url) insert the info from this function
 
 
-#load all the packages
-app = Flask(__name__)   #load all the packages ??? where does it look for packages
-
-#this says if the search bar shows /(just base url) insert the info from this function
 @app.route("/", methods=["GET"])
 # defining function that will get my info for page from github
 def index():
@@ -32,7 +28,9 @@ def index():
 
     # create a dictionary and assign it a key of repos and a value of the info from second api
     context = {
-        'repos': data2, 'general': data #also assign it another key of general and value of repos info, repos and general are the keys and data2, data are the values.
+        'repos': data2, 'general': data
+        # also assign it another key of general and value of repos info, repos
+        # and general are the keys and data2, data are the values.
     }
 
     print(type(context["repos"]))
@@ -40,33 +38,30 @@ def index():
     # in index file which we will be using for repos page, and replace the {% %} values with
     # key : value data that we call out of the api data se
     return render_template('index.html', **context)
-
-
-
-
-######## how to make tab sent tp followers , not to self in slack , put in html ############
+# how to make tab sent tp followers , not to self in slack , put in html ############
 
 #############################################################################
 
 
-#if query string has followers included bc the tab was clicked ( /followers endpoint after base url
+# if query string has followers included bc the tab was clicked ( /followers endpoint after base url
 # it will GET (using get method)info from api
 # an api is literally a collection of data and these particular  apis are lists of dictionaries
-
 @app.route('/followers/', methods=["GET"])
 
+
 def following_people():
-    response = requests.get("https://api.github.com/users/glaxur/followers" )
+    response = requests.get("https://api.github.com/users/glaxur/followers")
+
     data_followers = response.json()
 
     specific_user_data = []
+
     for user in data_followers:
         # user_url is a list
         user_url = user.get('url')
 
-        response2 = requests.get(user_url, )
+        response2 = requests.get(user_url)
         u_data = response2.json()
-
 
         specific_user_data.append(u_data)
 
@@ -75,9 +70,3 @@ def following_people():
         "followers": specific_user_data}
 
     return render_template('followers.html', **info_followers)
-
-#
-# use this is pipenv shell and no restarting of server required
-# export FLASK_APP=my_application
-# $ export FLASK_ENV=development
-# $
